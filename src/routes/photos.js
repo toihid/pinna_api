22a88272fdb411b7133d86639bebe9867667f502
Khandaker Toihidul Islam <it.toihid@gmail.com>
2025-08-27 12:02:32 +0200

    fetch last 4 images

--- src/routes/photos.js
+++ src/routes/photos.js
@@ -1,16 +1,21 @@
-import express from 'express';
-import Photo from '../models/Photo.js';
+import express from "express";
+import Photo from "../models/Photo.js";
 
 const router = express.Router();
 
 // GET /photos
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const photos = await Photo.find().sort({ createdAt: -1 });
+    //const photos = await Photo.find().sort({ createdAt: -1 });
+    const photos = await Photo.find()
+      .sort({ createdAt: -1 }) // newest first
+      .limit(4) // last 4 images
+      .allowDiskUse(true); // required for large documents in Atlas
+
     res.json(photos);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to fetch photos' });
+    res.status(500).json({ error: "Failed to fetch photos" });
   }
 });
 
